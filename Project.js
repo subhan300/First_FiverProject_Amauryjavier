c365eb28132fd2e10a3c4551a1fade3c0cdd81b4
Muhammad Subhan Akram <64373402+subhan300@users.noreply.github.com>
2021-03-22 00:58:00 +0500

    completed first fiver project
    i have done the required changes you want, now you can download the code from here.

---
+++ Project.js
@@ -0,0 +1,11 @@
+
+
+
+// function to control colors
+function fetch(){
+    var get=document.getElementById("get").value;
+    console.log(get,"get")
+   
+    document.getElementById("form").style.backgroundColor=get
+    document.getElementById("img2").style.backgroundColor=get
+}
